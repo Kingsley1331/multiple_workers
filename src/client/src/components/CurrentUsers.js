e40e1d27215b6f7089ca3b64ddf384c219ca07d4
Kingsley Ankomah <kingsley.ankomah@gmail.com>
2021-06-05 02:33:00 +0100

    Count sockets per appointment id

--- src/client/src/components/CurrentUsers.js
+++ src/client/src/components/CurrentUsers.js
@@ -1,11 +1,16 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { io } from "socket.io-client";
 
 let socket;
 
-export const CurrentUsers = () => {
+export const CurrentUsers = (props) => {
   const [members, setMembers] = useState([]);
   const [numberOfUsers, setNumberOfUsers] = useState(0);
+  const [otherUsers, setOthersUsers] = useState({});
+  const { appointmentId } = useParams();
+  // console.log("props", props);
+  console.log("===============================================");
 
   useEffect(() => {
     socket = io("http://localhost:4000");
@@ -13,37 +18,61 @@ export const CurrentUsers = () => {
   }, []);
 
   useEffect(() => {
-    setTimeout(() => {
-      if (socket.disconnected) {
-        socket = io("http://localhost:4000");
-      }
+    if (socket.disconnected) {
+      socket = io("http://localhost:4000");
+    }
 
+    setTimeout(() => {
       socket.emit("custom-event", "Kingsley Ankomah", socket.id);
+
+      socket.emit("send-appointment-data", {
+        appointmentId,
+        socketId: socket.id,
+      });
+
       socket.on("number-of-users", (numOfUsers) => {
-        console.log("SET PAGE MEMBERS LISTENER");
+        console.log("SET PAGE MEMBERS LISTENER1");
         setNumberOfUsers(numOfUsers);
       });
+
+      socket.on("appointment-data", (data) => {
+        console.log("data => ", data);
+        setOthersUsers(data);
+      });
     }, 500);
-  }, [setNumberOfUsers]);
+
+    return () => {
+      console.log(
+        "============================================socket disconnected2"
+      );
+      socket.disconnect();
+    };
+  }, [setNumberOfUsers, appointmentId]);
 
   useEffect(() => {
-    console.log("SET PAGE MEMBERS LISTENER");
+    console.log("SET PAGE MEMBERS LISTENER2");
     socket.on("page-members", (id) => {
       setMembers((list) => [...list, id]);
     });
     return () => {
+      console.log(
+        "============================================socket disconnected"
+      );
       socket.disconnect();
-      console.log("socket disconnected");
     };
   }, [setMembers]);
 
   return (
     <div className="App">
       <h1>Socket ids</h1>
-      {numberOfUsers > 1 && (
+      {otherUsers[appointmentId]?.length > 1 && (
         <h2>This appointment is currently being worked on!</h2>
       )}
-      <h3>{`Number of users: ${numberOfUsers}`}</h3>
+      {/* {numberOfUsers > 1 && (
+        <h2>This appointment is currently being worked on!</h2>
+      )} */}
+      <h3>{`Number of users: ${otherUsers[appointmentId]?.length}`}</h3>
+      {/* <h3>{`Number of users: ${numberOfUsers}`}</h3> */}
       <ul>
         {members.map((item) => (
           <li>{item}</li>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const express = require("express");
 
 const io = require("socket.io")(4000, {
@@ -6,33 +7,53 @@ const io = require("socket.io")(4000, {
   },
 });
 
+let appointments = {};
+
+const removeSocketId = (appointmentIds, socketId) => {
+  for (let id in appointmentIds) {
+    const socketIds = appointmentIds[id];
+    appointmentIds[id] = socketIds.filter((id) => id !== socketId);
+  }
+  return appointmentIds;
+};
+
 io.on("connection", (socket) => {
   console.log("socket id ==> ", socket.id);
+
   socket.on("custom-event", (name, id) => {
     console.log({ name, id });
     io.emit("page-members", id);
-    // socket.broadcast.emit("page-activity", name);
-    // console.log("number-of-users", io.engine.clientsCount);
+
     const numOfUsers = io.engine.clientsCount;
     io.emit("number-of-users", numOfUsers);
     console.log("number-of-users", numOfUsers);
-    // io.emit("page-activity", name);
   });
-  //   console.log("number-of-users", io.sockets);
+
+  socket.on("send-appointment-data", ({ appointmentId, socketId }) => {
+    console.log({ appointmentId, socketId });
+    if (appointments[appointmentId]?.length) {
+      appointments[appointmentId].push(socketId);
+    } else {
+      appointments[appointmentId] = [socketId];
+    }
+    io.emit("appointment-data", appointments);
+  });
+
   io.emit("number-of-users", io.engine.clientsCount);
-  /** WORKS! */
-  //   console.log("number-of-users", Object.keys(io.engine.clients));
-  //   console.log("number-of-users", io.engine.clientsCount);
-  //   console.log(
-  //     "number-of-users",
-  //     Object.keys(io.sockets.server.eio.clients).length
-  //   );
-  //   console.log("number-of-users", io.sockets.adapter.sids);
+
+  socket.on("disconnecting", function () {
+    console.log("=============================DISCONNECTING");
+    const socketId = socket.rooms.values().next().value; // NOTE: socket.rooms. is a Set
+    appointments = removeSocketId(appointments, socketId);
+    console.log("appointments ", appointments);
+    console.log("rooms", socketId);
+    io.emit("appointment-data", appointments);
+  });
+
   socket.on("disconnect", function (connection) {
     console.log("connection => ", connection);
     const decrement = connection === "transport close" ? 0 : 1;
-    //   socket.emit("disconnected");
-    // const numOfUsers = Object.keys(io.engine.clients).length - 1;
+
     const numOfUsers = io.engine.clientsCount - decrement;
     io.emit("number-of-users", numOfUsers);
     console.log("DISCONNECT number-of-users", numOfUsers);
